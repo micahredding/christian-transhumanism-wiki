b753f349486abf4c9a4c316597f15c2cc3f364c7
Micah Redding <micahtredding@gmail.com>
2014-05-26 13:28:04 -0500

    Initial commit

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+micahredding:$1$AOvVaa55$iHqKqQHSVevJE1jDp/uef/:Micah Redding:micahtredding@gmail.com:admin,user
+hormelbrandfoods:$1$nnSAqj6k$tVVmNXVCNBBDnfWUs2o710:Hormel Brand:hormelbrandfoods@gmail.com:user
